bcd3756db5d12b0a34ad45e265cf35a8f472a392
VIVEKSRM <vivek.ranjan.srm@gmail.com>
2024-12-28 21:32:49 +0530

    Java Concept Practice - Abstract Class

---
+++ src/test/java/javaPracticle/javaPracticle.java
@@ -0,0 +1,36 @@
+package javaPracticle;
+
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class javaPracticle {
+
+    @Test
+    public void RemoveDuplicateFromArrayList()
+    {
+        Integer[] arr={1,2,3,4,5,61,2,3,4,5};
+        ArrayList<Integer> ar=new ArrayList<>(Arrays.asList(arr));
+        ar.add(1);
+        ar.add(2);
+        System.out.println(ar);
+        LinkedHashSet<Integer> lh=new LinkedHashSet<>(ar);
+        System.out.println(lh);
+        System.out.println("-------------------");
+        for(Integer i: lh)
+            System.out.print(i);
+    }
+
+    @Test
+    public void convertArrayListToHashSet()
+    {
+        ArrayList<Integer> arr=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,4,5,6));
+        HashSet<Integer> hs=new HashSet<>(arr);
+        System.out.println(hs);
+    }
+
+}
