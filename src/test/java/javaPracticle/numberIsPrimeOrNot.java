5655bf292741c60f8b8e4bdf3f7654759fd410cf
VIVEKSRM <vivek.ranjan.srm@gmail.com>
2024-12-29 14:43:55 +0530

    Java Concept Practice - Abstract Class

--- src/test/java/javaPracticle/numberIsPrimeOrNot.java
+++ src/test/java/javaPracticle/numberIsPrimeOrNot.java
@@ -8,16 +8,17 @@ import java.util.*;
 public class numberIsPrimeOrNot {
     public static void main(String[] arg) {
         Scanner sc = new Scanner(System.in);
-        int j=0;
+        int j;
         boolean prime=true;
         System.out.println(" Please Enter a Number --: ");
         int number=sc.nextInt();
         sc.close();
         for (int i=2; i<=number/2; i++) {
             j = number % i;
-            if (j==0)
-                prime=false;
-            break;
+            if (j==0) {
+                prime = false;
+                break;
+            }
         }
             if(prime)
                 System.out.println("Given Number is Prime");
