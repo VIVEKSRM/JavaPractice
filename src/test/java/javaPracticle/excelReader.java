33b6d218b5f7497b3f28544a595c74fb45d9bc32
VIVEKSRM <vivek.ranjan.srm@gmail.com>
2025-01-11 14:43:52 +0530

    Java Concept Practice - Abstract Class

--- src/test/java/javaPracticle/excelReader.java
+++ src/test/java/javaPracticle/excelReader.java
@@ -13,89 +13,73 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class excelReader {
-	
+
 	//Identify Testcases coloum by scanning the entire 1st row
 	//once coloumn is identified then scan entire testcase coloum to identify purcjhase testcase row
 	//after you grab purchase testcase row = pull all the data of that row and feed into test
-	
+
 	public ArrayList<String> getData(String testcaseName) throws IOException
 	{
 		//fileInputStream arguments
-				ArrayList<String> a=new ArrayList<String>();
-				
-				FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\Project_Planing.xlsx");
+		ArrayList<String> a=new ArrayList<String>();
+
+		FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\Project_Planing.xlsx");
 		System.out.println(System.getProperty("user.dir")+"\\src\\test\\resources\\Project_Planing.xlsx");
-				XSSFWorkbook workbook=new XSSFWorkbook(fis);
-				
-				int sheets=workbook.getNumberOfSheets();
-				for(int i=0;i<sheets;i++)
-				{
-					if(workbook.getSheetName(i).equalsIgnoreCase("exampleReference"))
-							{
-					XSSFSheet sheet=workbook.getSheetAt(i);
-					//Identify Testcases coloum by scanning the entire 1st row
-					
-					 Iterator<Row>  rows= sheet.iterator();// sheet is collection of rows
-					Row firstrow= rows.next();
-					Iterator<Cell> ce=firstrow.cellIterator();//row is collection of cells
-					int k=0;
-					int coloumn = 0;
+		XSSFWorkbook workbook=new XSSFWorkbook(fis);
+
+		int sheets=workbook.getNumberOfSheets();
+		for(int i=0;i<sheets;i++)
+		{
+			if(workbook.getSheetName(i).equalsIgnoreCase("exampleReference"))
+			{
+				XSSFSheet sheet=workbook.getSheetAt(i);
+				//Identify Testcases coloum by scanning the entire 1st row
+				Iterator<Row>  rows= sheet.iterator();// sheet is collection of rows
+				Row firstrow= rows.next();
+				Iterator<Cell> ce=firstrow.cellIterator();//row is collection of cells
+				int k=0;
+				int coloumn = 0;
 				while(ce.hasNext())
 				{
 					Cell value=ce.next();
-					
 					if(value.getStringCellValue().equalsIgnoreCase("HeaderCol4"))
 					{
 						coloumn=k;
-						
 					}
-					
 					k++;
 				}
 				System.out.println(coloumn);
-				
 				////once coloumn is identified then scan entire testcase coloum to identify purcjhase testcase row
 				while(rows.hasNext())
 				{
-					
 					Row r=rows.next();
-					
 					if(r.getCell(coloumn).getStringCellValue().equalsIgnoreCase(testcaseName))
 					{
-						
 						////after you grab purchase testcase row = pull all the data of that row and feed into test
-						
 						Iterator<Cell>  cv=r.cellIterator();
 						while(cv.hasNext())
 						{
-						Cell c=	cv.next();
-						if(c.getCellType()==CellType.STRING)
-						{
-							
-						a.add(c.getStringCellValue());
-						}
-						else{
-							
-							a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
-						
-						}
+							Cell c=	cv.next();
+							if(c.getCellType()==CellType.STRING)
+							{
+								a.add(c.getStringCellValue());
+							}
+							else{
+								a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+							}
 						}
 					}
-					
-				
 				}
-
-							}
-				}
-				return a;
-				
+			}
+		}
+		return a;
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		excelReader er=new excelReader();
 		er.getData("row4");
-		
+
 	}
 
 }
