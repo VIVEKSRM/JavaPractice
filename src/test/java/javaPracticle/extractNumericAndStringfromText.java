a9b799e7ea1edcc18f0c97f11bff6e7a1fd87640
VIVEKSRM <vivek.ranjan.srm@gmail.com>
2026-01-09 12:41:56 +0530

    Java Concept Practice - Abstract Class

--- src/test/java/javaPracticle/extractNumericAndStringfromText.java
+++ src/test/java/javaPracticle/extractNumericAndStringfromText.java
@@ -3,23 +3,46 @@ package javaPracticle;
 import java.util.regex.*;
 public class extractNumericAndStringfromText {
         public static void main(String[] args) {
-//Check if the String is Numeric or not
+// Check if the String is Numeric or not
             String str = "1234573204";
+            System.out.println("Check if the String is Numeric or not-------------");
             System.out.println(str.matches("\\d+"));
-
             str = "vivek123ranjan456test";
 // Fetch only Numeric values from String
             String[] strarr = str.split("\\d+");
-
-            for(String str1:strarr)
+            System.out.println("Fetch only Numeric values from String-------------");
+            for (String str1 : strarr)
                 System.out.println(str1);
-
 // Fetch only String from the text
-            Pattern pattern=Pattern.compile("\\d+");
-            Matcher matcher=pattern.matcher(str);
-            while (matcher.find()){
+            System.out.println("Fetch only String from the text-------------");
+            Pattern pattern = Pattern.compile("\\d+");
+            Matcher matcher = pattern.matcher(str);
+            while (matcher.find()) {
                 System.out.println(matcher.group());
             }
+// Split with spaces
+            String text = "Hello world this is Vivek";
+            System.out.println("Split by spaces-------------");
+            String[] words = text.split(" ");
+            for (String word : words) {
+                System.out.println(word);
+            }
+// Split with tabs, multiple spaces
+            text = "Hello world this is Vivek";
+            System.out.println("Split by spaces,tabs, multiple spaces -------------");
+            words = text.split("\\s+"); // here '\\s' will replace only one space and '\\s+' will replace multiple spaces
+            for (String word : words) {
+                System.out.println(word);
+            }
+// Split by lines
+            text = "Hello world\nThis is Vivek\nAutomation rocks";
+            String[] lines = text.split("\n");
+            System.out.println("Split by lines-------------");
+            for (String line : lines) {
+                words = line.split(" ");
+                for (String word : words) {
+                    System.out.println(word);
+                }
+            }
         }
-
 }
