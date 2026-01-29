aa14fdb147ffd3344cd10d5e8ef06026bd489b2d
VIVEKSRM <vivek.ranjan.srm@gmail.com>
2026-01-29 08:53:13 +0530

    Java Concept Practice

--- src/test/java/javaPracticle/fibonacciSeries.java
+++ src/test/java/javaPracticle/fibonacciSeries.java
@@ -5,20 +5,37 @@ import java.util.Scanner;
 public class fibonacciSeries {
 
     public static void main(String[] arg) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the Number :- ");
+//        int number = sc.nextInt();
+//        sc.close();
+//        int a = 0;
+//        int b = 1;
+//        int temp;
+//        System.out.print(a + ", " + b + ", ");
+//        for (int i = 0; i <= number - 3; i++) {
+//            temp = a + b;
+//            System.out.print(temp + ", ");
+//            a = b;
+//            b = temp;
+//        }
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number :- ");
+        System.out.print("Enter the number of terms: ");
         int number = sc.nextInt();
         sc.close();
+
         int a = 0;
         int b = 1;
-        int temp;
-        System.out.print(a + ", " + b + ", ");
-        for (int i = 0; i <= number - 3; i++) {
-            temp = a + b;
-            System.out.print(temp + ", ");
+
+        System.out.print("Fibonacci Series: ");
+        for (int i = 1; i <= number; i++) {
+            System.out.print(a + " ");
+            int temp = a + b;
             a = b;
             b = temp;
         }
+
 
     }
 
